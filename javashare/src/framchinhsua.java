0e703d94ff681a75dfd67104969c8c44dca53325
tongoclam <tongoclam1@gmai.com>
2016-11-02 15:45:55 +0700

    init project

---
+++ javashare/src/framchinhsua.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author US
+ *
+ */
+public class framchinhsua extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		framchinhsua a = new framchinhsua();
+		a.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		a.setVisible(true);
+
+	}
+
+}
